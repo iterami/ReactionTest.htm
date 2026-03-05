1d991ba846f87b9c1b255fd64e5627da5b0cf1e4
Jan Hořava <czechinthemail@gmail.com>
2026-03-05 06:58:52 -0800

    added checkbox label

--- repo.js
+++ repo.js
@@ -26,7 +26,7 @@ function repo_init(){
       'storage': {
         'audio': true,
       },
-      'storage_menu': '<table><tr><td><input id=audio type=checkbox><td>Audio</table>',
+      'storage_menu': '<table><tr><td><input id=audio type=checkbox><td><label for=audio>Audio</label></table>',
       'title': 'ReactionTest.htm',
       'ui_elements': [
         'box',
